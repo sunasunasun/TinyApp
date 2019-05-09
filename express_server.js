03c5430d3be92590fe5ad945c47a32f688ab896e
sunasunasun <xiaoyu.sun@ucalgary.ca>
2019-05-09 01:48:24 +0000

    add logout

--- express_server.js
+++ express_server.js
@@ -40,6 +40,11 @@ app.post("/login", (req, res) => {
   res.redirect("/urls");
 });
 
+app.post("/logout", (req, res) => {
+  res.clearCookie('username');
+  res.redirect("/urls");
+});
+
 app.post('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL
   console.log(shortURL, req.body.longURL)
